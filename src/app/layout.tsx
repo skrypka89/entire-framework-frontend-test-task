e2d3ee2a16a47008db671e5905dbb5e1c80cfdcf
Oleksandr Skrypka <skrypka.o.v@gmail.com>
2023-07-18 14:45:12 +0300

    Initialize project

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next';
+import { IBM_Plex_Mono } from 'next/font/google';
+
+import '@/styles/index.sass';
+
+const font = IBM_Plex_Mono({
+  weight: ['400'],
+  style: ['normal'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: 'Menu Items',
+  description: 'A test task from Entire Framework company',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import styles from '@/app/page.module.sass';
+
+export default function Home() {
+  return <main className={styles.main}></main>;
+}
